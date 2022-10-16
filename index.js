6afdff06d61a35170996357638c0bbeaf3c5309c
dwidge <dwidge+gh@gmail.com>
2022-10-16 20:36:17 +0200

    feat(index): export array

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-export circle from "./svg/circle.svg";
-export square from "./svg/square.svg";
+import circle from "./circle.svg";
+import diamond from "./diamond.svg";
+import kite from "./kite.svg";
+import oval from "./oval.svg";
+import parallelogram from "./parallelogram.svg";
+import semicircle from "./semicircle.svg";
+import square from "./square.svg";
+import triangle from "./triangle.svg";
+
+export {
+  circle,
+  diamond,
+  kite,
+  oval,
+  parallelogram,
+  semicircle,
+  square,
+  triangle,
+};
+
+const all = [
+  circle,
+  diamond,
+  kite,
+  oval,
+  parallelogram,
+  semicircle,
+  square,
+  triangle,
+];
+export default all;
